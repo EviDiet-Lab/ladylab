8bc61c9bf9773d0161f0ed644ba53dad2ff6af47
ThreeRock69 <baselabo.kyoto@gmail.com>
2026-06-18 22:36:25 +0900

    Add Search Console verification file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 // Google Analytics ID（本番時はここに設定）
-const GA_ID = ''; // 'G-XXXXXXXXXX'
+const GA_ID = 'G-4YKNW7950N';
 
 export default defineConfig({
   site: 'https://EviDiet-Lab.github.io',
